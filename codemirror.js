32b35f138078a77f46e41c6bb54ff727003f2b94
Kate Shcherbak <katescherbak@gmail.com>
2015-07-31 16:11:59 +0300

    multiplexmode

--- codemirror.js
+++ codemirror.js
@@ -1,5 +1,18 @@
 define([
         'lib/codemirror',
         'css!lib/codemirror',
-        'mode/twig/twig'
-    ],function(){});
+        'mode/twig/twig',
+        'mode/multiplex'
+    ],function(CodeMirror){
+
+    CodeMirror.defineMode("twig_html", function (config) {
+        return CodeMirror.multiplexingMode (
+            CodeMirror.getMode(config, "text/html"), {
+                open: /\{[%#]/, close: /[%#]\}/,
+                mode: CodeMirror.getMode(config, "twig"),
+                parseDelimiters: true
+            });
+    });
+
+    return CodeMirror
+});
